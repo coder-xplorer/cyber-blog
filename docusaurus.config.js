f8db0f210215749e05665e3804608b53c8445546
fe-u <1214517669@qq.com>
2024-03-27 11:18:06 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,11 +13,11 @@ const config = {
   favicon: 'img/Bob.jpg',
 
   // Set the production url of your site here
-  url: 'https://coder-xplorer.github.io/cyber-blog/',
+  url: 'https://coder-xplorer.github.io',
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cyber-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
