854334ae0ee01007bf200bb868be412fb40e3ec7
fe-u <1214517669@qq.com>
2024-03-27 13:43:47 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Record from '@site/src/components/Record';
-import List from './help';
+
+const List = [
+  {
+    title: 'Iframe 通信',
+    tags: ['前端'],
+    content:
+      '在工作中，我们会遇到一些需求，使用 iframe 嵌套另一个页面，这个页面大部分情况下不会部署在父页面相同的域名下，但是又需要父子页面进行数据交互，那么我们该怎么处理呢？',
+    href: '/docs/iframe-communication',
+  },
+  {
+    title: '无线端开发（H5）注意事项',
+    tags: ['前端', 'React'],
+    content: '无线端开发注意事项，点击阅读更多。',
+    href: '/docs/wireless',
+  },
+  {
+    title: 'Web Component',
+    tags: ['WebComponent', 'React'],
+    content: 'WebComponent（原生组件）的一些用法梳理。',
+    href: '/docs/web-component',
+  },
+];
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
